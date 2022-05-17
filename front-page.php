357f6cea8bd50f145bf36fe19ace81eea9e66cbc
TribalDav <tribaldav@gmail.com>
2022-05-17 16:18:28 +0200

    ajout ACF, gutenberg, customisation theme perso

--- front-page.php
+++ front-page.php
@@ -5,8 +5,18 @@ get_header();
 
 if ( have_posts() ) : 
     while ( have_posts() ) : the_post(); ?>
+    <div class="container">
         <h2><?php the_title() ?></h2>
-        <span class="pills"><?php the_category() ?></span>
+        <div><?php the_content() ?></div>
+        <div class="row">
+            <div class="col">
+                <img class="img-fluid" src="<?php the_field('image_a_gauche')?>" alt="la plus belle image">
+            </div>
+            <div class="col">
+                <p><?php the_field('texte_a_droite') ?></p>
+            </div>
+        </div>
+        </div>    
     <?php endwhile; 
 endif; 
 get_footer();
--- functions.php
+++ functions.php
@@ -9,9 +9,25 @@ function mon_theme_scripts(){
     wp_enqueue_style( 'style', get_stylesheet_uri() );
     wp_enqueue_script( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js', [], false, true);
 }
-
+function mon_theme_custom_types(){
+    register_post_type('boardgame', [
+        'labels' => [
+            'name' => 'Boardgames',
+            'singular_name' => 'Boardgame'
+        ],
+        'public' => true,
+        'menu_position' => 3,
+        'menu_icon' => 'dashicons-buddicons-activity',
+        'show_in_rest' => true,
+        'supports' => [
+            'title', 'editor', 'thumbnail'
+        ],
+        'has_archive' => true
+    ]);
+}
 
 
 
 add_action('after_setup_theme', 'montheme_setup');
+add_action('init', 'mon_theme_custom_types');
 add_action('wp_enqueue_scripts', 'mon_theme_scripts');
