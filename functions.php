69fcf56fc520b857135856e07467a2d00816a3d5
TribalDav <tribaldav@gmail.com>
2022-05-18 16:32:33 +0200

    ajout fonctions, link, taxo, navbar

--- functions.php
+++ functions.php
@@ -2,14 +2,16 @@
 
 function montheme_setup(){
     add_theme_support('post-thumbnails');
+    add_theme_support('custom-logo');
+    register_nav_menu('main', 'menu_principal');
 }
 
-function mon_theme_scripts(){
+function montheme_scripts(){
     wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css');
     wp_enqueue_style( 'style', get_stylesheet_uri() );
     wp_enqueue_script( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js', [], false, true);
 }
-function mon_theme_custom_types(){
+function montheme_custom_types(){
     register_post_type('boardgame', [
         'labels' => [
             'name' => 'Boardgames',
@@ -22,12 +24,37 @@ function mon_theme_custom_types(){
         'supports' => [
             'title', 'editor', 'thumbnail'
         ],
-        'has_archive' => true
+        'has_archive' => true,
+        'taxonomies' => ['gameCats']
+    ]);
+}
+function montheme_custom_taxonomies(){
+    register_taxonomy('gameCats', 'boardgame', [
+        'labels' => [
+            'name' => 'gameCats',
+            'singular_name' => 'gameCat'
+        ],
+        'public' => true,
+        'hierarchical' => true,
+        'show_admin_column' => true,
+        'show_in_rest' => true,
+        'default_term' => 'Uncategorised'
     ]);
 }
 
+function montheme_menu_cls($classes){
+    $classes[]= 'nav-item';
+    return $classes;
+}
 
+function montheme_menu_link_atts($attributes){
+    $attributes['class']= 'nav-link';
+    return $attributes;
+}
 
 add_action('after_setup_theme', 'montheme_setup');
-add_action('init', 'mon_theme_custom_types');
-add_action('wp_enqueue_scripts', 'mon_theme_scripts');
+add_action('init', 'montheme_custom_types');
+add_action('init', 'montheme_custom_taxonomies');
+add_action('wp_enqueue_scripts', 'montheme_scripts');
+add_filter('nav_menu_css_class', 'montheme_menu_cls');
+add_filter('nav_menu_link_attributes', 'montheme_menu_link_atts');
