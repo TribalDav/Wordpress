f1e3a92c69847b497121cc9d1658a5ab6cd3d19b
Alex TRAN <alex.tran87280@gmail.com>
2022-05-19 09:55:03 +0200

    ajout des pages archive, single et taxonomy

--- front-page.php
+++ front-page.php
@@ -3,21 +3,21 @@
 get_header();
 
 
-if ( have_posts() ) : 
-    while ( have_posts() ) : the_post(); ?>
-    <div class="container">
-        <h2><?php the_title() ?></h2>
-        <div><?php the_content() ?></div>
-        <div class="row">
-            <div class="col">
-                <img class="img-fluid" src="<?php the_field('image_a_gauche')?>" alt="la plus belle image">
-            </div>
-            <div class="col">
-                <p><?php the_field('texte_a_droite') ?></p>
+if (have_posts()) :
+    while (have_posts()) : the_post(); ?>
+        <div class="container">
+            <h2 class="text-center"><?php the_title() ?></h2>
+            <div><?php the_content() ?></div>
+            <div class="row">
+                <div class="col">
+                    <img class="img-fluid" src="<?php the_field('image_a_gauche') ?>" alt="la plus belle image">
+                </div>
+                <div class="col">
+                    <p><?php the_field('texte_a_droite') ?></p>
+                </div>
             </div>
         </div>
-        </div>    
-    <?php endwhile; 
-endif; 
+<?php endwhile;
+endif;
 get_footer();
 ?>
---
+++ single-boardgame.php
@@ -0,0 +1,20 @@
+<?php
+get_header();
+
+if (have_posts()) :
+    while (have_posts()) : the_post(); ?>
+        <div class="container">
+            <h2 class="text-center"><?php the_title() ?></h2>
+            <?php the_post_thumbnail();  ?>
+            <div><?php the_content() ?></div>
+            <div>Date de publication : <?php the_field('date_de_publication') ?></div>
+            <div>Nombre de joueurs : <?= the_field('nombre_de_joueur') ?> </div>
+            <div>Age conseillé : <?= the_field('age') ?> </div>
+            <div>Durée de la partie : <?= the_field('duree_de_la_partie') ?> </div>
+        </div>
+<?php endwhile;
+endif;
+
+get_footer();
+
+?>
