03cb374637e998312a3541288adc935fd4c07f64
TribalDav <tribaldav@gmail.com>
2022-05-20 12:16:26 +0200

    modif page 404, récup code après crash

--- 404.php
+++ 404.php
@@ -4,9 +4,9 @@
 
 
 <div class="conteneur">
-
-<h1>ERREUR 666</h1>
-<h2>Nous nous reverrons en enfer</h2>
+<img src="http://localhost:8080/site_vitrine_exo/wp-content/uploads/2022/05/Devil-Transparent-Image.png" alt="Devil Inside">
+<h1 id="erreurCode">ERREUR 666</h1>
+<h2 id="erreurMsg">Nous nous reverrons en enfer</h2>
 
 </div>
 
