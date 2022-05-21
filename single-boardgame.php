aae07e6eaac1c98627f52c2921b8c941b97d72b3
lafeehalima <hcm2@hotmail.fr>
2022-05-21 09:55:23 +0200

    message

--- single-boardgame.php
+++ single-boardgame.php
@@ -7,98 +7,16 @@ if (have_posts()) :
             <h2 class="text-center"><?php the_title() ?></h2>
             <?php the_post_thumbnail();  ?>
             <div id="texte"><?php the_content() ?></div>
-            <div class="haut" id="date">Date de publication : <?php the_field('date_de_publication') ?></div>
-            <div class="haut">Nombre de joueurs : <?= the_field('nombre_de_joueur') ?> </div>
-            <div class="haut">Age conseillé : <?= the_field('age') ?> </div>
-            <div class="haut">Durée de la partie : <?= the_field('duree_de_la_partie') ?> </div>
+            <div id="prix">Prix : <?php the_field('prix')?></div>
+            <div id="date">Date de publication : <?php the_field('date_de_publication') ?></div>
+            <div id="joueur">Nombre de joueurs : <?= the_field('nombre_de_joueurs') ?> </div>
+            <div id="age">Age conseillé : <?= the_field('age') ?> </div>
+            <div id="temps">Durée de la partie : <?= the_field('duree_de_la_partie') ?> </div>
             <div><?= the_taxonomies() ?></div>
             <hr>
         </div>
 <?php endwhile;
 endif; ?>
-
-<!-- barre de navigation haut -->
-<div class="w3-top">
-  <div class="w3-bar w3-white w3-card" id="myNavbar">
-    <a href="#home" class="w3-bar-item w3-button w3-wide">LOGO</a>
-    <!-- liens de la barre de navigation de droite -->
-    <div class="w3-right w3-hide-small">
-      <a href="#about" class="w3-bar-item w3-button">ABOUT</a>
-      
-      <a href="#work" class="w3-bar-item w3-button"><i class="fa fa-th"></i> WORK</a>
-      
-      <a href="#contact" class="w3-bar-item w3-button"><i class="fa fa-envelope"></i> CONTACT</a>
-    </div>
-    <!-- masque les liens flottants à droite sur les petits écrans et remplace par icone de menu -->
-
-    <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="w3_open()">
-      <i class="fa fa-bars"></i>
-    </a>
-  </div>
-</div>
-
-
-<!-- Header avec image pleine hauteur -->
-<header class="bgimg-1 w3-display-container w3-grayscale-min" id="home">
-  <div class="w3-display-left w3-text-white" style="padding:48px">
-    <span class="w3-jumbo w3-hide-small">Start something that matters</span><br>
-    <span class="w3-xxlarge w3-hide-large w3-hide-medium">Start something that matters</span><br>
-    <span class="w3-large">Stop wasting valuable time with projects that just isn't you.</span>
-    <p><a href="#about" class="w3-button w3-white w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off">Learn more and start today</a></p>
-  </div> 
-  <div class="w3-display-bottomleft w3-text-grey w3-large" style="padding:24px 48px">
-    <i class="fa fa-facebook-official w3-hover-opacity"></i>
-    <i class="fa fa-instagram w3-hover-opacity"></i>
-    <i class="fa fa-snapchat w3-hover-opacity"></i>
-    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-    <i class="fa fa-twitter w3-hover-opacity"></i>
-    <i class="fa fa-linkedin w3-hover-opacity"></i>
-  </div>
-</header>
-
-
-
-
-
-
-
-
-
-<!-- images en taille réelle au clic-->
-<div id="modal01" class="w3-modal w3-black" onclick="this.style.display='none'">
-  <span class="w3-button w3-xxlarge w3-black w3-padding-large w3-display-topright" title="Close Modal Image">×</span>
-  <div class="w3-modal-content w3-animate-zoom w3-center w3-transparent w3-padding-64">
-    <img id="img01" class="w3-image">
-    <p id="caption" class="w3-opacity w3-large"></p>
-  </div>
-</div>
-
-
-
-
-<!-- Contact -->
-<div class="w3-container w3-light-grey" style="padding:128px 16px" id="contact">
-  <h3 class="w3-center">CONTACT</h3>
-  <p class="w3-center w3-large">Lets get in touch. Send us a message:</p>
-  <div style="margin-top:48px">
-    <p><i class="fa fa-map-marker fa-fw w3-xxlarge w3-margin-right"></i> Chicago, US</p>
-    <p><i class="fa fa-phone fa-fw w3-xxlarge w3-margin-right"></i> Phone: +00 151515</p>
-    <p><i class="fa fa-envelope fa-fw w3-xxlarge w3-margin-right"> </i> Email: mail@mail.com</p>
-    <br>
-    <form action="/action_page.php" target="_blank">
-      <p><input class="w3-input w3-border" type="text" placeholder="Name" required name="Name"></p>
-      <p><input class="w3-input w3-border" type="text" placeholder="Email" required name="Email"></p>
-      <p><input class="w3-input w3-border" type="text" placeholder="Subject" required name="Subject"></p>
-      <p><input class="w3-input w3-border" type="text" placeholder="Message" required name="Message"></p>
-      <p>
-        <button class="w3-button w3-black" type="submit">
-          <i class="fa fa-paper-plane"></i> SEND MESSAGE
-        </button>
-      </p>
-    </form>
-    
-  </div>
-</div>
 
 
  
