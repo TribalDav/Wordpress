67b5683d961387ca1b69a4cadc194ee751e00711
alexafpa2022 <alex_tran93@yahoo.fr>
2022-05-24 08:35:48 +0200

    hello

--- home.php
+++ home.php
@@ -3,30 +3,37 @@
 get_header();
 $boardgame = new WP_Query([
     'post_type' => 'boardgame',
-    'order' => 'ASC'
 ]);
+// print_r($boardgame);
+
 ?>
 <div class="container">
     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 my-3">
         <?php
         if ($boardgame->have_posts()) :
             while ($boardgame->have_posts()) : the_post();
-                $boardgame->the_post();
+                if (isset($boardgame)) :
+                    $boardgame->the_post();
+
         ?>
 
-                <div class="col-3">
-                    <div class="card shadow bg-body rounded">
-                        <a href="<?= the_permalink() ?>">
-                            <div class="text-center p-1"><?= the_post_thumbnail() ?></div>
-                        </a>
-                        <div class="card-body">
-                            <h5 class="card-title text-center"><?= the_title() ?></h5>
-                            <p class="card-text"><?= the_excerpt() ?></p>
+                    <div class="col-3">
+                        <div class="card shadow bg-body rounded">
+                            <a href="<?= the_permalink() ?>">
+                                <div class="text-center p-1"><?= the_post_thumbnail() ?></div>
+                            </a>
+                            <div class="card-body">
+                                <h5 class="card-title text-center"><?= the_title() ?></h5>
+                                <p class="card-text"><?= the_excerpt() ?></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-        <?php endwhile;
+        <?php
+                endif;
+            endwhile;
+            wp_reset_postdata();
         endif;
+
         ?>
     </div>
 </div>
